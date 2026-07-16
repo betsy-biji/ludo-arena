d2f28b8b202c3f7f42e5ff915fe50b9609e69931
betsy-biji <betsybiji3436@gmail.com>
2026-07-16 12:32:06 +0530

    Configure frontend for production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-
- baseURL:
- "http://localhost:5000/api"
-
+  baseURL: "https://ludo-arena-production-aa34.up.railway.app/api",
 });
 
 export default api;
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://ludo-arena-production-aa34.up.railway.app", {
   autoConnect: false,
   transports: ["websocket"],
 });
